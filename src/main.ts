850967038140b497696d5df6bd072322a559a116
Mateus Furquim <furquim.axs@gmail.com>
2023-05-08 12:41:13 -0300

    Invoking Rust function in Typescript

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { invoke } from '@tauri-apps/api'
 
 createApp(App).mount('#app')
+
+// now we can call our Command!
+// Right-click the application background and open the developer tools.
+// You will see "Hello, World!" printed in the console!
+invoke('greet', { name: 'World' })
+  // `invoke` returns a Promise
+  .then((response) => console.log(response))
